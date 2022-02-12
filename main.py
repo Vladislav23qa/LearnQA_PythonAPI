adf656f34cadaab6d1aad5c3f3471aa2e204c9bd
Dell <vladteste@ukr.net>
2022-02-12 20:41:26 +0200

    new scripts

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
+from json.decoder import JSONDecodeError
+#response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "User"})
+#parsed_response_text = response.json()
+#print(parsed_response_text["answer"])
 
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-response1 = requests.get("https://playground.learnqa.ru/api/get_text")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
-print(response1)
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not a Json format")
